05d97f01b0ef3eb13542f92163e205122b6e01f6
Repp Denis <Drepp@aplana.com>
2014-01-14 20:29:56 +0500

    tests

---
+++ src/test/java/ru/repp/chat/server/ServerTest.java
@@ -0,0 +1,18 @@
+package ru.repp.chat.server;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author @Drepp
+ * @since 14.01.14
+ */
+public class ServerTest {
+
+
+    @Test
+    public void testStartup() {
+        System.out.println("test start!");
+        Assert.assertTrue(true);
+    }
+}
